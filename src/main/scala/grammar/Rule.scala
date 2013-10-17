824caa072eda49f43f6ccad7cb9c713acf814b0a
a-sir <alexander.sirenko@gmail.com>
2013-10-17 10:50:14 +0700

    Fixed case-classes inheritance.

--- src/main/scala/grammar/Rule.scala
+++ src/main/scala/grammar/Rule.scala
@@ -4,4 +4,8 @@ package grammar
  * @author A.Sirenko
  *          Date: 9/8/13
  */
-case class Rule(left: List[GSym], right: List[GSym], cost: Double)
+class Rule(val left: List[GSym], val right: List[GSym], val cost: Double) {
+
+	override def toString = "left: " + left + ", right: " + right + "cost: " + cost
+
+}
