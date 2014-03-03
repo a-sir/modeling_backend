cc085a8a85daa9618db04f6e9619982045a97d97
a-sir <alexander.sirenko@gmail.com>
2014-03-03 13:42:46 +0700

    Replaced dictionary

--- src/main/scala/import_ling/CognemReader.scala
+++ src/main/scala/import_ling/CognemReader.scala
@@ -1,9 +1,8 @@
 package import_ling
 
-import java.util.zip.GZIPInputStream
-import java.io._
 import cognems.Cognem
 import util.StringUtils._
+import scala.collection.JavaConverters._
 
 /**
  * @author A.Sirenko
@@ -11,55 +10,7 @@ import util.StringUtils._
  */
 object CognemReader {
 
-	val DEFAULT_WIKITIONARY_PARSED = "data/wikitionary.parsed.gz"
-
-	lazy val defaultSet : List[Cognem] = read(DEFAULT_WIKITIONARY_PARSED)
-
-	def parse(lines: List[String]): List[Cognem] = {
-		if (!lines.head.startsWith("# ") || lines.length < 2) {
-			throw new RuntimeException("Wrong lines: " + lines)
-		}
-		val name = lines.head.substring(2)
-		var res: List[Cognem] = List()
-		for (i <- 1 to lines.length - 1) {
-			val comp = lines(i).split('|')
-			val desc = comp(1)
-			var areas: Array[String] = comp(0).split(',')
-			if (areas.length == 1 && areas(0) == "---") {
-				areas = Array.empty
-			}
-			res = new Cognem(name, desc, areas) :: res
-		}
-		res
-	}
-
-	def read(path: String): List[cognems.Cognem] = {
-		var cognems: List[Cognem] = List()
-		var currentCognRaw: List[String] = List()
-		val br = new BufferedReader(new InputStreamReader(
-			new GZIPInputStream(new FileInputStream(path))
-		))
-		var line: String = ""
-		while (true) {
-			line = br.readLine
-			if (line == null || line.startsWith("# ")) {
-				if (!currentCognRaw.isEmpty) {
-					cognems = CognemReader.parse(currentCognRaw.reverse) ::: cognems
-				}
-				if (line == null) {
-					return cognems
-				}
-				currentCognRaw = List(line)
-			} else {
-				if (currentCognRaw.isEmpty) {
-					throw new RuntimeException("Description of cognema was met before it's name")
-				} else {
-					currentCognRaw = line :: currentCognRaw
-				}
-			}
-		}
-		throw new RuntimeException("Missed EOF")
-	}
+	lazy val defaultSet : List[Cognem] = Cognem.loadDefault().asScala.toList
 
 	def filterCognemsByChars(cognems: List[Cognem]): List[Cognem] = {
 		cognems.filter(
