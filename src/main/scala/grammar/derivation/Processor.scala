54da6c0006349c5772b047f8cb7395af68461890
a-sir <alexander.sirenko@gmail.com>
2014-06-26 20:39:22 +0700

    Project built.

--- src/main/scala/grammar/derivation/Processor.scala
+++ src/main/scala/grammar/derivation/Processor.scala
@@ -12,29 +12,28 @@ import akka.actor._
 
 class Processor (
   val pool: ExecutorService = Executors.newSingleThreadExecutor(),
-  val requests: Set[String] = Set.empty
+  var requests: Set[String] = Set.empty
 ) extends Actor {
 
-  override def receive(msg: Object) {
-    msg match {
-      case s: String =>
-        if (s.startsWith("submit:")) {
-          val sessionQuery = s.substring(7)
-          if (requests.contains(sessionQuery)) {
-            println("Request" + sessionQuery + "is submitted already")
-          } else {
-            val del = data.indexOf(":")
-            val sessionId = data.substring(0, del);
-            val query =data.substring(del + 1)
-            pool.submit(new ProcessTask(sessionId, query, new Array[Actor]{sender; this}))
-            requests += data
-          }
+  override def receive = {
+    case s: String =>
+      if (s.startsWith("submit:")) {
+        val sessionQuery = s.substring(7)
+        if (requests.contains(sessionQuery)) {
+          println("Request" + sessionQuery + "is submitted already")
+        } else {
+          val del = sessionQuery.indexOf(":")
+          val sessionId = sessionQuery.substring(0, del);
+          val query = sessionQuery.substring(del + 1)
+          val receivers: List[ActorRef] = List(sender, this.sender())
+          pool.submit(new ProcessTask(sessionId, query, receivers))
+          requests += sessionQuery
         }
-      case res: Array[String] =>
-        println("Query" + res.mkString(":") + " is done")
-        requests -= res(0)
-      case _ => println("Unknown message")
-    }
+      }
+    case res: Array[String] =>
+      println("Query" + res.mkString(":") + " is done")
+      requests -= res(0)
+    case _ => println("Unknown message")
   }
 
   def close {
@@ -42,10 +41,17 @@ class Processor (
   }
 }
 
-class ProcessTask (val sessionId: String, val query: String, val receivers: Array[Actor]) extends Callable[Array[String]] {
-  def call(): Array[String] = {
-    val res = Array[String] {sessionId; "Failed"}
-    receiver ! res
-    res
+class ProcessTask (
+  val sessionId: String, val query: String, val receivers: List[ActorRef])
+    extends Runnable {
+  def run() = {
+    val res: Array[String] = Array(sessionId, "Failed")
+    receivers.foreach {_ ! res}
+  }
+
+  def process() {
+    // prepare GSym from grammar, prepare query and send to deriv
+    // deriv.compute(query:Query)
+    println("Done")
   }
 }
