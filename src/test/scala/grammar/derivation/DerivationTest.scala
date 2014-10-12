4e4883a1b8b185ae4ca2a013a59cb14cd8df00dd
a-sir <alexander.sirenko@gmail.com>
2014-10-13 00:32:27 +0700

    Fix test

--- src/test/scala/grammar/derivation/DerivationTest.scala
+++ src/test/scala/grammar/derivation/DerivationTest.scala
@@ -9,50 +9,50 @@ import util.TestUtils
  */
 
 class DerivationTest extends FunSpec {
-	describe("A Derivation") {
+    describe("A Derivation") {
 
-    it("should produce correct chains for english letters") {
-      val grammar = Grammar.createMock()
-      val sentence = grammar.syms.getOrCreateSymbols(List("a", "c"))
+        it("should produce correct chains for english letters") {
+            val grammar = Grammar.createMock()
+            val sentence = grammar.syms.getOrCreateSymbols(List("a", "c"))
 
-      val deriv = Derivation.createForStrings
-      val reached = deriv.compute(new Query(sentence, grammar, 5, 5, 3))
-      assert(!reached.aggrSyms.keys.isEmpty)
-      val reachedLetters = reached.aggrSyms.keys.foldLeft(Set[String]())((s, v) => s + v.name)
-      assert(TestUtils.areSame(reachedLetters, Set("b", "d")))
-      val reachedSyms = reached.aggrSyms
-      for ((sym: GSym, derivDetails: AggrDerivSym) <- reachedSyms) {
-        Console.out.println("Symbol " + sym.getKey + ": " + sym.name)
-        for (chain <- derivDetails.chains) {
-          Console.out.println("Chain " + sym + ": " + chain)
+            val deriv = Derivation.createForStrings
+            val reached = deriv.compute(new Query(sentence, grammar, 5, 5, 3))
+            assert(!reached.aggrSyms.keys.isEmpty)
+            val reachedLetters = reached.aggrSyms.keys.foldLeft(Set[String]())((s, v) => s + v.name)
+            assert(TestUtils.areSame(reachedLetters, Set("b", "d")))
+            val reachedSyms = reached.aggrSyms
+            for ((sym: GSym, derivDetails: AggrDerivSym) <- reachedSyms) {
+                Console.out.println("Symbol " + sym.getKey + ": " + sym.name)
+                for (chain <- derivDetails.chains) {
+                    Console.out.println("Chain " + sym + ": " + chain)
+                }
+            }
         }
-      }
-    }
 
-    it("should return symbols with chains") {
-      val grammar = Grammar.createMock()
-      val sentence = grammar.syms.getOrCreateSymbols(List("a", "c"))
+        it("should return symbols with chains") {
+            val grammar = Grammar.createMock()
+            val sentence = grammar.syms.getOrCreateSymbols(List("a", "c"))
 
-      val deriv = Derivation.createForStrings
-      val reached = deriv.compute(new Query(sentence, grammar, 5, 5, 3))
-      assert(!reached.aggrSyms.keys.isEmpty)
-      val reachedLetters = reached.aggrSyms.keys.foldLeft(Set[String]())((s, v) => s + v.name)
-      assert(TestUtils.areSame(reachedLetters, Set("b", "d")))
-      val reachedSyms = reached.aggrSyms
-      for ((sym: GSym, derivDetails: AggrDerivSym) <- reachedSyms) {
-        Console.out.println("Symbol " + sym.getKey + ": " + sym.name)
-        for (chain <- derivDetails.chains) {
-          Console.out.println("Chain " + sym + ": " + chain)
+            val deriv = Derivation.createForStrings
+            val reached = deriv.compute(new Query(sentence, grammar, 5, 5, 3))
+            assert(!reached.aggrSyms.keys.isEmpty)
+            val reachedLetters = reached.aggrSyms.keys.foldLeft(Set[String]())((s, v) => s + v.name)
+            assert(TestUtils.areSame(reachedLetters, Set("b", "d")))
+            val reachedSyms = reached.aggrSyms
+            for ((sym: GSym, derivDetails: AggrDerivSym) <- reachedSyms) {
+                Console.out.println("Symbol " + sym.getKey + ": " + sym.name)
+                for (chain <- derivDetails.chains) {
+                    Console.out.println("Chain " + sym + ": " + chain)
+                }
+            }
         }
-      }
-    }
 
         it("should operate on English language") {
             val grammar = Grammar.createEnglishGrammar()
             val deriv = Derivation.createForDictionary(grammar)
             val querySyms = grammar.getSymbols(grammar.lemmatizer.tokenizeAndLemmatize("Russian apple", false))
 
-            val result = deriv.compute(new Query(querySyms, grammar, 10, 10, 5))
+            val result = deriv.compute(new Query(querySyms, grammar, 10, 25, 5))
             assert(result.aggrSyms.keys.size > 12 && result.aggrSyms.keys.size < 25)
 
             val names = result.aggrSyms.keys.foldLeft(Set.empty[String])((a,b)=> a + b.name)
@@ -62,7 +62,7 @@ class DerivationTest extends FunSpec {
             )
             shouldBe.foreach(x => assert(names.contains(x)))
         }
-        
+
         it("should operate long-running query") {
             val grammar = Grammar.createEnglishGrammar()
             val deriv = Derivation.createForDictionary(grammar)
@@ -73,5 +73,5 @@ class DerivationTest extends FunSpec {
 
             val names = result.aggrSyms.keys.foldLeft(Set.empty[String])((a,b)=> a + b.name)
         }
-	}
+    }
 }
