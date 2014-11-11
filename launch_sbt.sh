76c33b9e89c3502c6c600eef0eb9e885049adfd2
a-sir <alexander.sirenko@gmail.com>
2014-11-11 13:26:42 +0700

    Remove redundant launch script.

--- launch_sbt.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-export SBT_OPTS="-Xmx1536M -Xms512M -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=2G -Xss2M -Duser.timezone=GMT"
-
-java -Dsbt.ivy.home=./.ivy2/cache -jar ./lib/sbt-launch.jar "$@"
